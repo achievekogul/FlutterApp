c4e93489b79602b46bbe26b56931930aea6f26a6
achievekogul <achievekogul231@gmail.com>
2023-07-06 09:44:54 +0530

    Push

--- FlutterApp-PlayGround/lib/test.dart
+++ FlutterApp-PlayGround/lib/test.dart
@@ -3,34 +3,32 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-        title: 'Flutter First App',
-        theme: ThemeData(useMaterial3: true),
-    home: Scaffold(
-    appBar: AppBar(
-    title: const Text('Flutter layout demo'),
-    backgroundColor: Colors.blue,
-    ),
-    body: InkWell(
-      child: Container(
-        width: 500.0,
-        padding: new EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 40.0),
-        color: Colors.green,
-        child: new Column(
-            children: [
+      title: 'Flutter First App',
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+          backgroundColor: Colors.blue,
+        ),
+        body: InkWell(
+          child: Container(
+            width: 500.0,
+            padding: const EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 40.0),
+            color: Colors.green,
+            child: Column(children: [
               new Text("Ableitungen"),
-            ]
+            ]),
+          ),
+          onTap: () {
+            print("Tapped on container");
+          },
         ),
       ),
-      onTap: () {
-        print("Tapped on container");
-      },
-    ),
-    ),
     );
   }
 }
