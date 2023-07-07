7a5a6b62e0ae6a63a5436e432cbf293d83bffd80
achievekogul <achievekogul231@gmail.com>
2023-07-07 11:22:25 +0530

    WebPageNew

---
+++ lib/web.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WebApp());
+}
+
+class WebApp extends StatelessWidget {
+  const WebApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                  Color.fromRGBO(177, 33, 57, 1),
+                  Color.fromRGBO(20, 10, 27, 1)
+                ])),
+                child: const Column(
+                  children: [
+                    Text(
+                      'Home Page',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ))));
+  }
+}
